d4ca8ac4b1d76123d4938d4d271c7bbe51e97428
Madper Xie <xiechj@mail2.sysu.edu.cn>
2020-01-16 17:25:13 +0800

    clean up

--- lehmer.go
+++ lehmer.go
@@ -1,5 +1,6 @@
 package lehmer
 
+import "encoding/binary"
 
 type Reader struct {
 	lehmer64 uint64
@@ -23,25 +24,17 @@ func putUint64(b []byte, v uint64) {
 	b[7] = byte(v >> 56)
 }
 
-func (r *Reader)Read(buf []byte) (n int, err error) {
+func (r Reader)Read(buf []byte) (n int, err error) {
 	l := len(buf)
 	c := l / 8
-	m := l % 8
-	n = 0
+	// m := l % 8
 	for i := 0; i < c; i++ {
 		r.lehmer64 *= 0xda942042e4dd58b5;
-		buf[i*8+0] = byte(r.lehmer64)
-		buf[i*8+1] = byte(r.lehmer64 >> 8)
-		buf[i*8+2] = byte(r.lehmer64  >> 16)
-		buf[i*8+3] = byte(r.lehmer64  >> 24)
-		buf[i*8+4] = byte(r.lehmer64  >> 32)
-		buf[i*8+5] = byte(r.lehmer64  >> 40)
-		buf[i*8+6] = byte(r.lehmer64  >> 48)
-		buf[i*8+7] = byte(r.lehmer64  >> 56)
+		binary.LittleEndian.PutUint64(buf[i*8:i*8+8], r.lehmer64)
 	}
-	r.lehmer64 *= 0xda942042e4dd58b5;
-	for i := 0; i < m; i++ {
-		buf[8*c+i] = byte(r.lehmer64 >> 8*uint64(i))
-	}
+	// r.lehmer64 *= 0xda942042e4dd58b5;
+	// for i := 0; i < m; i++ {
+	// 	buf[8*c+i] = byte(r.lehmer64 >> 8*uint64(i))
+	// }
 	return l, nil
 }
